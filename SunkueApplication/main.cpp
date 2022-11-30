caac6db21e57e819ed06146ee86683806db07f35
Yoon Sunkue <wmwwmmm@naver.com>
2022-11-30 19:14:50 +0900

    example

--- SunkueApplication/main.cpp
+++ SunkueApplication/main.cpp
@@ -3,7 +3,7 @@
 #include "Application/Object.h"
 
 // xyz or pwn
-// 샘플용으로 만든 간단한 파일 임포트 함수에요. 꼭 이걸 쓸 필요가 없어요
+// 샘플용으로 만든 간단한 PCD 파일 임포트 함수에요. 꼭 이걸 쓸 필요는 없어요.
 std::pair<std::vector<Eigen::Vector3d>, std::vector<Eigen::Vector3d>> ReadPcdFile(const std::string& fileName) {
 	auto dotPos = fileName.find_last_of(".");
 	auto extStr = fileName.substr(dotPos + 1);
@@ -32,25 +32,6 @@ std::pair<std::vector<Eigen::Vector3d>, std::vector<Eigen::Vector3d>> ReadPcdFil
 
 int main()
 {
-	/*
-	// 58 번째 줄 부터 보면 됩니다.
-	Helper::Directory::SetMainDir("D:\\");
-	auto analyzer = anal3(Helper::Directory::AutoDir("_샘플PCD\\Raw221103_ec20_ov1_vc1.75_in.pwn"));
-	In3D::Model::Kernel::In3DModelKernelAccessor::Up2dateData(analyzer.getModel(),
-		In3D::Model::Kernel::eBaseSystem::OP3D, In3D::Model::Kernel::eDataSetType::PCD);
-	auto& op3dPcdData = analyzer.getModel().getOp3DPcdData();
-
-	auto mainProcessedModel =
-		Process::In3DMainProcessPipeline{ analyzer.getModel() }
-		.cgalPoisson(16, 0.15, 0, 500).elapsed("poisson::")
-		.getModel();
-
-	In3D::Model::Kernel::In3DModelKernelAccessor::Up2dateData(mainProcessedModel,
-		In3D::Model::Kernel::eBaseSystem::OP3D, In3D::Model::Kernel::eDataSetType::MESH);
-	auto& op3dMeshData = mainProcessedModel.getOp3DMeshData();
-
-	*/
-
 	auto data = ReadPcdFile("D:\\_샘플PCD\\Raw221103_ec20_ov1_vc1.75_in.pwn");
 
 	// 윈도우 창을 생성한다. 
@@ -63,26 +44,51 @@ int main()
 	// Object 를 생성한다.
 	auto pcd = In3D::App::CreatePcdObject("pcd", data.first, data.second);
 
-	
-//	auto pcd = In3D::App::CreatePcdObject("pcd", op3dPcdData->points_, op3dPcdData->normals_);
-//	auto mesh = In3D::App::CreateMeshObject("mesh", op3dMeshData->vertices_, op3dMeshData->vertex_normals_, op3dMeshData->triangles_);
-	
-
 	// std::string 과 vector<double> 로 Feature 를 만든다.
 	// 각각 이름과 정보이다.
-//	for (const auto& f : analyzer.GetFeatureMap()) {
-//		pcd.AddFeature(f.first, f.second);
-//	}
+	std::vector<double> s1; s1.reserve(data.first.size()); for (int i = 0; i < s1.size(); i++)s1.push_back(i);
+	In3D::App::Values   s2; s2.reserve(data.first.size()); for (int i = 0; i < s1.size(); i++)s1.push_back(0.01 * (i % 1000));
+	pcd.AddFeature("sample1", s1);
+	pcd.AddFeature("sample2", s2);
 
 	// 지금 디스플레이 할 피쳐를 이름을 통해 선택한다.
-	pcd.SetActivefeature("Distance");
+	pcd.SetActivefeature("sample1");
 
 	// 화면에 그릴 친구들은 여기 등록한다.  // Draw 함수가 매 프레임 불린다.
-//	win.renderer()->Regist(mesh);
 	win.renderer()->Regist(pcd);
 
 	// GUI 를 사용할 친구들은 여기 등록한다. // DrawGui 함수가 매 프레임 불린다.
-//	win.gui()->Regist(mesh);
+	win.gui()->Regist(pcd);
+	win.gui()->Regist(win.renderer()->mainCamera());
+	win.gui()->Regist(win.renderer()->sun());
+
+	// 렌더러에 등록되어있는 오브젝트들을 모두 한 화면에 그려질 수 있도록 해준다. R 키를 누르면 이 함수가 불린다.
+	win.renderer()->ResetCamera();
+
+	// 업데이트 루프를 실행한다.
+	win.run();
+}
+
+
+// 아래는 또 다른 예시. (여러개 등록 및 메쉬 띄우기)
+int main2()
+{
+	/*
+	SunkueWindow win(1240, 720);
+	Shader::ShaderDir() = "Application/src/shader";
+
+	auto pcd = In3D::App::CreatePcdObject("pcd", op3dPcdData->points_, op3dPcdData->normals_);
+	auto mesh = In3D::App::CreateMeshObject("mesh", op3dMeshData->vertices_, op3dMeshData->vertex_normals_, op3dMeshData->triangles_);
+
+	for (const auto& f : analyzer.GetFeatureMap()) {
+		pcd.AddFeature(f.first, f.second);
+	}
+	pcd.SetActivefeature("Distance");
+
+	win.renderer()->Regist(mesh);
+	win.renderer()->Regist(pcd);
+
+	win.gui()->Regist(mesh);
 	win.gui()->Regist(pcd);
 	win.gui()->Regist(win.renderer()->mainCamera());
 	win.gui()->Regist(win.renderer()->sun());
@@ -94,13 +100,12 @@ int main()
 	// auto& min = m * item..bound().minimum();
 	// auto& max = m * item..bound().maximum();
 	// transform 이 적용된 바운드는 위와 같이 구해서 사용하면 된다.
-//mesh.transform().translate({ mesh.bound().size().x() * 1.5f, 0, 0 });
+	mesh.transform().translate({ mesh.bound().size().x() * 1.5f, 0, 0 });
 
-	// 렌더러에 등록되어있는 오브젝트들을 모두 한 화면에 그려질 수 있도록 해준다. R 키를 누르면 이 함수가 불린다.
 	win.renderer()->ResetCamera();
 
-	// 업데이트 루프를 실행한다.
 	win.run();
+	*/
 }
 
 
